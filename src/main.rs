7f934b95f4da75a8cebc6bbd9756fb62dfcb83ff
jyy <1305081045@qq.com>
2025-02-26 20:58:11 +0800

    feature: build  skeleton of project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+
+async fn greet(req: HttpRequest) -> impl HttpResponder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
