969140a304f6e55aff6c6d26505b79bdfdc0f841
jyy <1305081045@qq.com>
2025-02-28 09:19:23 +0000

    feature: implement subscribe api by extractint form data

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,24 @@ async fn health_check() -> impl Responder {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| 
+        App::new()
+        .route("/health_check", web::get().to(health_check))
+        .route("/subscriptions", web::post().to(subscribe))
+    )
         .listen(listener)?
         .run();
 
     Ok(server)
+}
+
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
