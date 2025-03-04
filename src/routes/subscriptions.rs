5b8cdc6ea39a4212a5ab2f0129a1d6f4c64eb6ce
jyy <1305081045@qq.com>
2025-03-04 04:22:31 +0000

    feature: import tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,22 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> impl Responder {
     let request_id = Uuid::new_v4();
-    log::info!("request_id {} - Adding '{}' '{}' as a new subscriber.",
-            request_id,
-            form.email,
-            form.name
+    // Spans, like logs,have an associated level
+    // `info_span` creates a span at the info_level
+    // 我们不再使用字符串插值：跟踪允许我们将结构化信息作为键值对集合关联到我们的跨度中。
+    // 我们可以明确命名它们（例如subscriber_email用于form.email），或者隐式使用变量名作为键（例如独立的request_id等同于request_id = request_id）。
+    // 我们在它们前面都加了一个%符号：这是告诉tracing使用它们的Display实现来进行日志记录
+    let request_span = tracing::info_span!("Adding a new subscriber.",
+            %request_id,
+            subscriber_email =  %form.email,
+            subscriber_name  =  %form.name
     );
-    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
+    //using `enter` in an async function is not a good idea
+    //using `enter` there is just 
+    // `info_span` 返回新创建的 span，但我们必须显式使用 `.enter()` 方法进入它以激活它。
+    // `.enter()` 返回一个 `Entered` 实例，这是一个守卫：只要守卫变量未被释放，所有下游的跨度和日志事件都将被注册为已进入跨度的子项。
+    let _request_span_guard = request_span.enter();
+    tracing::info!("request_id {} - Saving new subscriber details in the database", request_id);
     let result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id,email,name,subscribed_at)
@@ -32,11 +42,11 @@ pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>)
 
     match result {
         Ok(_) => {
-        log::info!("request_id {} - New subscriber details have been saved", request_id);
+        tracing::info!("request_id {} - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish() 
         },
         Err(e) => {
-            log::error!("request_id {} - Failed to execute query: {}", request_id, e);
+            tracing::error!("request_id {} - Failed to execute query: {}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
